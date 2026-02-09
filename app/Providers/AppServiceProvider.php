d0f50f644ad6e311b6b89070bc3f56af458cdf79
Jeremy Duarte <jeremydamian.db@gmail.com>
2026-02-09 03:52:16 -0500

    refactor: organizacion carpetas migrations closes #28
    - provider configurado con rutas personalizadas
    - estructura de app para las migraciones (revisar issue #28 si quieres saber más)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->loadMigrationsFrom([
+            database_path('migrations/laravel_basic'),
+            database_path('migrations/finance'),
+            database_path('migrations/restaurant'),
+            database_path('migrations/rooms'),
+            database_path('migrations/users'),
+    ]);
     }
 }
