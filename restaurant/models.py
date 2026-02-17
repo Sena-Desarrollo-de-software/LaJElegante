9a5e292d264249f2e2cb715bd95a14690681873c
Jeremy Duarte <jeremydamian.db@gmail.com>
2026-02-17 16:12:07 -0500

    feat: modelos para aplicación restaurant closes #42
    - modelos con auditoria implementada
    - 3 modelos funcionales para restaurant
    - referencia a cliente en reserva restaurante hace falta (no hay modelo)

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class BaseAuditModel(models.Model):
+    is_active = models.BooleanField(default=True)  # Soft delete
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+# === Mesa ===
+class Mesa(BaseAuditModel):
+    numero_mesa = models.SmallIntegerField()
+    capacidad = models.SmallIntegerField()
+    zona = models.CharField(max_length = 50)
+    ubicacion_detalle = models.CharField(max_length = 150)
+
+# === Reserva Restaurante ===
+class ReservaRestaurante(BaseAuditModel):
+    #Referencia temporal a cliente
+    cliente_id = models.PositiveIntegerField()
+    mesa = models.ForeignKey(
+        Mesa,
+        on_delete = models.CASCADE,
+        related_name = 'reserva', 
+    )
+    hora_reserva = models.TimeField()
+    numero_personas = models.SmallIntegerField()
+
+    ESTADO_RESERVA_CHOICES = [
+        ('PENDIENTE','pendiente'),
+        ('CONFIRMADA','confirmada'),
+        ('CANCELADA','cancelada'),
+    ]
+
+    estado_reserva = models.CharField(
+        max_length = 20,
+        choices = ESTADO_RESERVA_CHOICES,
+        default =  'PENDIENTE',
+    )
