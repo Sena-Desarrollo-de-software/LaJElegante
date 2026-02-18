17085c7d0d8a5ba6762a9576c144f23c9e52d766
Jeremy Duarte <jeremydamian.db@gmail.com>
2026-02-17 20:14:45 -0500

    feat: modelos en app users closes #43
    - se extiende un modelo de AbstractUser de Django
    - se usaran grupos para manejar roles y permisos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#Se usaran los modelos de Django cada grupo = Rol
+class Usuario(AbstractUser):
+    # created_at = models.DateTimeField(auto_now_add=True) <- campo que esta en modelo User como data_joined
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    # is_active = models.BooleanField(default=True) <- ya esta en modelo User bajo el mismo nombre
