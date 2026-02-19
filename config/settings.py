562b0661235fdd2ffc51470a39a1c8c8e49f7703
Jeremy Duarte <jeremydamian.db@gmail.com>
2026-02-18 21:29:34 -0500

    chore: configuración settings.py con .env
    - las dependencias decouple:config ayudan a redireccionar las variables de entorno a .env (más seguro)
    - .env con configuraciones para SECRET_KEY,DEBUG,DATABASE y ALLOWED_HOST
    - la dependencia dj_database_url simplifica como pasar los strings de las bases de datos y la conexión entre varios tipos de motores

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c-(je4%_gj4+(28e*ih(pyptr7)3@q=e74!%k332b6=^v*evh9'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', 
+                       cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
@@ -79,10 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
