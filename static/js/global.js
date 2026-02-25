96a9dc3209a1267152df142a914fe0e3c9e3d744
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-02-25 12:35:19 -0500

    feat: archivos estaticos:js closes #82
    - javascript global para verificar el DOM funcionando, pequeño mensaje de bienvenida

---
+++ static/js/global.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM completamente cargado y listo');
+    
+    console.log('Hotel La J Elegante - Sistema cargado');
+});
