11a14d36fe670834403a2b7e2a3f7feafeb5a0c8
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-02-24 19:58:14 -0500

    chore: configuración de mensajes personalizados closes #78
    - 4 tipos de posibles respuestas para correos
    - configuración redirigida .env (ejemplo de .env x item)

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,9 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+FROM_EMAILS = {
+    'no-reply': config('FROM_EMAIL_NO_REPLY', default=DEFAULT_FROM_EMAIL),
+    'reservas': config('FROM_EMAIL_RESERVAS', default=DEFAULT_FROM_EMAIL),
+    'info': config('FROM_EMAIL_INFO', default=DEFAULT_FROM_EMAIL),
+    'ayuda': config('FROM_EMAIL_AYUDA', default=DEFAULT_FROM_EMAIL),
+}
