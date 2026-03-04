0070b78bc7f6dc637fdfddcdd9c541bbf982b028
JulianSuarez <jsse0706@gmail.com>
2026-03-03 21:39:33 -0500

    feat(core): mejorar TyC con componentes y estilos (#92)

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path("hotel/lobby/", views.lobby, name="lobby"),
+    path("tyc/", TemplateView.as_view(template_name="hotel/tyc.html"), name="tyc"),
 ]
