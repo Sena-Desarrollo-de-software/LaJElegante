3aa5527545da94a685acac470ddf068e650b429b
JulianSuarez <jsse0706@gmail.com>
2026-03-03 22:23:33 -0500

    feat(core): mejorar login con componentes y assets separados (#90)

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path("hotel/lobby/", views.lobby, name="lobby"),
+
     path("tyc/", TemplateView.as_view(template_name="hotel/tyc.html"), name="tyc"),
+
+    path("login/", TemplateView.as_view(template_name="hotel/login.html"), name="login"),
+
+    path("signup/", TemplateView.as_view(template_name="hotel/signup.html"), name="signup"),
 ]
