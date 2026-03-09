2847bd99c4495b15d1720fbb73a38896b37607ec
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-09 06:31:00 -0500

    refactor: constantes de auditoria en core.admin.py
    - mover constantes de auditoria a un lugar accesible para todas las apps
    - ahorrar escritura de codigo con un pequeño import

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+AUDITORIA_FIELDSET = ('Auditoría', {
+    'fields': ('created_at', 'updated_at', 'deleted_at'),
+    'classes': ('collapse',),
+})
+
+AUDITORIA_READONLY = ('created_at', 'updated_at', 'deleted_at')
