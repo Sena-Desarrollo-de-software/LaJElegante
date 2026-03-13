469fab5a19638552afae158b13e5b9027eea7f35
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-13 17:43:36 -0500

    refactor: proteger metodos de las vistas habitacion
    - esto es debido a que son un problema para la seguridad si no se definen los metodos permitidos en las vistas
    - protección CSRF, asegurando que no se usen metodos "inseguros"

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
 from .models import Habitacion
 from .forms import HabitacionCreateForm, HabitacionUpdateForm, HabitacionDeleteForm
 
 HABITACION_LIST_REDIRECT = "rooms:habitacion_list"
+
+@require_safe
+@login_required
 def habitacion_list(request):
     habitaciones = Habitacion.objects.filter(is_active=True).order_by("numero_habitacion")
     return render(request, "rooms/habitacion_list.html", {"habitaciones": habitaciones})
 
-
+@require_http_methods(["GET", "POST"])
+@login_required
+@csrf_protect
 def habitacion_create(request):
     if request.method == "POST":
         form = HabitacionCreateForm(request.POST)
@@ -20,6 +27,9 @@ def habitacion_create(request):
         form = HabitacionCreateForm()
     return render(request, "rooms/habitacion_create.html", {"form": form})
 
+@require_http_methods(["GET", "POST"])
+@login_required
+@csrf_protect
 def habitacion_update(request, pk):
     habitacion = get_object_or_404(Habitacion, pk=pk, is_active=True)
 
@@ -33,7 +43,9 @@ def habitacion_update(request, pk):
 
     return render(request, "rooms/habitacion_update.html", {"form": form, "habitacion": habitacion})
 
-
+@require_POST
+@login_required
+@csrf_protect
 def habitacion_delete(request, pk):
     habitacion = get_object_or_404(Habitacion, pk=pk, is_active=True)
 
