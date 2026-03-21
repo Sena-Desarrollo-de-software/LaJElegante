89eaaf5b045797de5a6b47753cfe218afd0ad224
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-21 16:47:52 -0500

    feat: templates para backoffice, archivos base
    - layout base para backoffice, con futuros archivos staticos incluidos
    - template dashboard, basico que conecta con sus componentes
    - estructura de componentes vacia para dashboard, componentes base: action_shorcut, sidebar y widget_query_container
    - urls y patterns para dashboard.html

--- backoffice/views.py
+++ backoffice/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET,require_http_methods
 
-# Create your views here.
+@require_GET
+def dasboard(request):
+    return render(request,'dashboard.html')
