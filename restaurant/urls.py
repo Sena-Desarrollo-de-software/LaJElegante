4842f729661845a77db4f4072e5393e876affb39
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-23 15:13:15 -0500

    feat(restaurant): infrastructura para templates
    - Views redireccionando a templates
    - URL patterns genericos con tipado paracido
    - Vistas genericas para HTML, prototipadas basadas en formulario CRUD con softdeletes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,4 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # === HORARIOS ===
+    path('horario/', views.index_horario, name='horario_index'),
+    path('horario-create/', views.create_horario, name='horario_create'),
+    path('horario-update/<int:pk>', views.update_horario, name='horario_update'),
+    path('horario-delete/<int:pk>', views.delete_horario, name='horario_delete'),
+    path('horario-trashcan/<int:pk>', views.trashcan_horario, name='horario_trashcan'),
+    # === MESAS ===
+    path('mesa/', views.index_mesa, name='mesa_index'),
+    path('mesa-create/', views.create_mesa, name='mesa_create'),
+    path('mesa-update/<int:pk>', views.update_mesa, name='mesa_update'),
+    path('mesa-delete/<int:pk>', views.delete_mesa, name='mesa_delete'),
+    path('mesa-trashcan/<int:pk>', views.trashcan_mesa, name='mesa_trashcan'),
+    # === RESERVA RESTAURANTE ===
+    path('reserva-restaurante/', views.index_reserva_restaurante, name='reserva_restaurante_index'),
+    path('reserva-restaurante-create/', views.create_reserva_restaurante, name='reserva_restaurante_create'),
+    path('reserva-restaurante-update/<int:pk>', views.update_reserva_restaurante, name='reserva_restaurante_update'),
+    path('reserva-restaurante-delete/<int:pk>', views.delete_reserva_restaurante, name='reserva_restaurante_delete'),
+    path('reserva-restaurante-trashcan/<int:pk>', views.trashcan_reserva_restaurante, name='reserva_restaurante_trashcan'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET,require_POST,require_http_methods
 
-# Create your views here.
+# === HORARIO ===
+@require_GET
+def index_horario(request):
+    return render(request,'horario_index.html')
+
+@require_POST
+def create_horario(request):
+    return render(request,'horario_create.html')
+
+@require_http_methods(['POST','GET'])
+def update_horario(request):
+    return render(request,'horario_update.html')
+
+@require_http_methods(['POST','GET'])
+def delete_horario(request):
+    return render(request, 'horario_delete.html')
+
+@require_http_methods(['POST','GET'])
+def trashcan_horario(request):
+    return render(request,'horario_trashcan.html')
+
+# === MESA ===
+@require_GET
+def index_mesa(request):
+    return render(request,'mesa_index.html')
+
+@require_POST
+def create_mesa(request):
+    return render(request,'mesa_create.html')
+
+@require_http_methods(['POST','GET'])
+def update_mesa(request):
+    return render(request,'mesa_update.html')
+
+@require_http_methods(['POST','GET'])
+def delete_mesa(request):
+    return render(request, 'mesa_delete.html')
+
+@require_http_methods(['POST','GET'])
+def trashcan_mesa(request):
+    return render(request,'mesa_trashcan.html')
+
+# === RESERVA RESTAURANTE ===
+@require_GET
+def index_reserva_restaurante(request):
+    return render(request,'reserva_restaurante_index.html')
+
+@require_POST
+def create_reserva_restaurante(request):
+    return render(request,'reserva_restaurante_create.html')
+
+@require_http_methods(['POST','GET'])
+def update_reserva_restaurante(request):
+    return render(request,'reserva_restaurante_update.html')
+
+@require_http_methods(['POST','GET'])
+def delete_reserva_restaurante(request):
+    return render(request, 'reserva_restaurante_delete.html')
+
+@require_http_methods(['POST','GET'])
+def trashcan_reserva_restaurante(request):
+    return render(request,'reserva_restaurante_trashcan.html')
