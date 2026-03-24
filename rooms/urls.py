7c949d1d67cfb5ca456ad58e4d1a1aa2ee252df2
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-23 19:40:38 -0500

    feat: infrastructura para app rooms
    - URL patterns para vistas de html vacias
    - Decoradores de django para views
    - Views de redirección

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,8 +4,22 @@ from . import views
 app_name = 'rooms'
 
 urlpatterns = [
-    path("", views.habitacion_list, name="habitacion_list"),
-    path("crear/", views.habitacion_create, name="habitacion_create"),
-    path("editar/<int:pk>/", views.habitacion_update, name="habitacion_update"),
-    path("eliminar/<int:pk>/", views.habitacion_delete, name="habitacion_delete"),
+    # === HABITACIÓN ===
+    path("habitacion/", views.index_habitacion, name="habitacion_index"),
+    path("habitacion-create/", views.create_habitacion, name="habitacion_create"),
+    path("habitacion-update/<int:pk>/", views.update_habitacion, name="habitacion_update"),
+    path("habitacion-delete/<int:pk>", views.delete_habitacion, name="habitacion_delete"),
+    path('habitacion-trashcan/<int:pk>', views.trashcan_habitacion, name='habitacion_trashcan'),
+    # === RESERVA HABITACIÓN ===
+    path("reserva-restaurante/", views.index_reserva_restaurante, name="reserva_restaurante_index"),
+    path("reserva-restaurante-create/", views.create_reserva_restaurante, name="reserva_restaurante_create"),
+    path("reserva-restaurante-update/<int:pk>/", views.update_reserva_restaurante, name="reserva_restaurante_update"),
+    path("reserva-restaurante-delete/<int:pk>", views.delete_reserva_restaurante, name="reserva_restaurante_delete"),
+    path('reserva-restaurante-trashcan/<int:pk>', views.trashcan_reserva_restaurante, name='reserva_restaurante_trashcan'),
+    # === TIPO HABITACIÓN
+    path("tipo-habitacion/", views.index_tipo_habitacion, name="tipo_habitacion_index"),
+    path("tipo-habitacion-create/", views.create_tipo_habitacion, name="tipo_habitacion_create"),
+    path("tipo-habitacion-update/<int:pk>/", views.update_tipo_habitacion, name="tipo_habitacion_update"),
+    path("tipo-habitacion-delete/<int:pk>", views.delete_tipo_habitacion, name="tipo_habitacion_delete"),
+    path('tipo-habitacion-trashcan/<int:pk>', views.trashcan_tipo_habitacion, name='tipo_habitacion_trashcan'),
 ]
