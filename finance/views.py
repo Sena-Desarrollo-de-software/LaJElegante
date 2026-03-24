faebfe22d6d8395bd1112edba669347c4b1f4983
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-23 22:14:02 -0500

    feat: infraestructura para finance
    - URL patterns para CRUD
    - Views con redireccionamiento
    - Organizacion de templates vacios

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods, require_POST, require_GET
 
-# Create your views here.
+# === IMPUESTO ===
+@require_GET
+def index_impuesto(request):
+    return render(request,'backoffice/impuestos/impuesto_index.html')
+
+@require_POST
+def create_impuesto(request):
+    return render(request,'backoffice/impuestos/impuesto_create.html')
+
+@require_http_methods(['POST','GET'])
+def update_impuesto(request):
+    return render(request,'backoffice/impuestos/impuesto_update.html')
+
+@require_http_methods(['POST','GET'])
+def delete_impuesto(request):
+    return render(request, 'backoffice/impuestos/impuesto_delete.html')
+
+@require_http_methods(['POST','GET'])
+def trashcan_impuesto(request):
+    return render(request,'backoffice/impuestos/impuesto_trashcan.html')
+
+# === TARIFA ===
+@require_GET
+def index_tarifa(request):
+    return render(request,'backoffice/tarifas/tarifa_index.html')
+
+@require_POST
+def create_tarifa(request):
+    return render(request,'backoffice/tarifas/tarifa_create.html')
+
+@require_http_methods(['POST','GET'])
+def update_tarifa(request):
+    return render(request,'backoffice/tarifas/tarifa_update.html')
+
+@require_http_methods(['POST','GET'])
+def delete_tarifa(request):
+    return render(request, 'backoffice/tarifas/tarifa_delete.html')
+
+@require_http_methods(['POST','GET'])
+def trashcan_tarifa(request):
+    return render(request,'backoffice/tarifas/tarifa_trashcan.html')
+
+# === TEMPORADA ===
+@require_GET
+def index_temporada(request):
+    return render(request,'backoffice/temporadas/temporada_index.html')
+
+@require_POST
+def create_temporada(request):
+    return render(request,'backoffice/temporada/temporada_create.html')
+
+@require_http_methods(['POST','GET'])
+def update_temporada(request):
+    return render(request,'backoffice/temporadas/temporada_update.html')
+
+@require_http_methods(['POST','GET'])
+def delete_temporada(request):
+    return render(request, 'backoffice/temporadas/temporada_delete.html')
+
+@require_http_methods(['POST','GET'])
+def trashcan_temporada(request):
+    return render(request,'backoffice/temporadas/temporada_trashcan.html')
