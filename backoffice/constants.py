9190e423451cefed074b3ccc44e090668df50f21
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-29 10:27:52 -0500

    feat(backoffice): context_proccesor para dashboard
    - Se desarrolla un context_proccesor para limitar el acceso por grupos de empleados
    - Se implementan constantes y codigo mantenible para manipular cualquier agregado

---
+++ backoffice/constants.py
@@ -0,0 +1,26 @@
+SIDEBAR_CONFIG = {
+    'Administrador': {
+        'finance': ['tarifa', 'impuesto', 'temporada'],
+        'restaurant': ['turno', 'horario', 'reservarestaurante'],
+        'rooms': ['tipohabitacion', 'habitacion', 'reservahabitacion'],
+        'users': ['usuario', 'group'],
+    },
+    'Gerente General': {
+        'finance': ['tarifa', 'impuesto', 'temporada'],
+        'restaurant': ['turno', 'horario', 'reservarestaurante'],
+        'rooms': ['tipohabitacion', 'habitacion', 'reservahabitacion'],
+    },
+    'Gerente de Habitaciones': {
+        'finance': ['tarifa', 'impuesto', 'temporada'],
+        'rooms': ['tipohabitacion', 'habitacion', 'reservahabitacion'],
+    },
+    'Gerente de Comidas y Bebidas': {
+        'finance': ['tarifa', 'impuesto', 'temporada'],
+        'restaurant': ['turno', 'horario', 'reservarestaurante'],
+    },
+    'Asistente Administrativo': {
+        'finance': ['tarifa', 'impuesto', 'temporada'],
+        'restaurant': ['turno', 'horario', 'reservarestaurante'],
+        'rooms': ['tipohabitacion', 'habitacion', 'reservahabitacion'],
+    },
+}
