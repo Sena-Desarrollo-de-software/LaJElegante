c259d923c3bee42ddd82e3e608d69f763a694088
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-29 20:22:25 -0500

    feat(backoffice): usar context proccesor para shortcut actions
    - Se usa un context proccesor para realizar la division de grupos, cada uno tendra sus acciones configuradas aparte
    - Esta implementacion es parecida a sidebar haciendo mucho mas sencillo el despliegue, usando variables globales
    - Se registra este context processor en settings

--- backoffice/constants.py
+++ backoffice/constants.py
@@ -29,3 +29,113 @@ SIDEBAR_CONFIG = {
         'rooms': ['tipohabitacion', 'habitacion', 'reservahabitacion'],
     },
 }
+
+ACTIONS = {
+    'RESERVA_CREATE': {
+        'label': 'Crear Reserva',
+        'icon': 'bi-plus-circle',
+        'url_name': 'backoffice:reserva_create',
+        'color': 'primary',
+    },
+    'RESERVA_LIST': {
+        'label': 'Ver Reservas',
+        'icon': 'bi-journal',
+        'url_name': 'backoffice:reserva_index',
+        'color': 'primary',
+    },
+    'HABITACION_CREATE': {
+        'label': 'Nueva Habitación',
+        'icon': 'bi-door-open',
+        'url_name': 'rooms:habitacion_create',
+        'color': 'primary',
+    },
+    'HABITACION_LIST': {
+        'label': 'Habitaciones',
+        'icon': 'bi-door-open',
+        'url_name': 'rooms:habitacion_index',
+        'color': 'primary',
+    },
+    'TARIFA_CREATE': {
+        'label': 'Nueva Tarifa',
+        'icon': 'bi-cash',
+        'url_name': 'finance:tarifa_create',
+        'color': 'primary',
+    },
+    'TARIFA_LIST': {
+        'label': 'Tarifas',
+        'icon': 'bi-cash',
+        'url_name': 'finance:tarifa_index',
+        'color': 'primary',
+    },
+    'USUARIO_LIST': {
+        'label': 'Usuarios',
+        'icon': 'bi-people',
+        'url_name': 'users:usuario_index',
+        'color': 'primary',
+    },
+    'DASHBOARD': {
+        'label': 'Dashboard',
+        'icon': 'bi-house-door',
+        'url_name': 'backoffice:dashboard',
+        'color': 'primary',
+    },
+    'TURNOS': {
+        'label': 'Turnos',
+        'icon': 'bi-clock',
+        'url_name': 'restaurant:turno_index',
+        'color': 'primary',
+    },
+    'RESERVA_RESTAURANTE_CREATE': {
+        'label': 'Nueva Reserva',
+        'icon': 'bi-plus-circle',
+        'url_name': 'restaurant:reserva_restaurante_create',
+        'color': 'primary',
+    },
+    'RESERVA_RESTAURANTE_LIST': {
+        'label': 'Reservas Restaurante',
+        'icon': 'bi-book',
+        'url_name': 'restaurant:reserva_restaurante_index',
+        'color': 'primary',
+    },
+}
+
+ROLE_ACTION_KEYS = {
+
+    'Administrador': [
+        'RESERVA_CREATE',
+        'RESERVA_LIST',
+        'HABITACION_CREATE',
+        'TARIFA_CREATE',
+        'USUARIO_LIST',
+        'DASHBOARD',
+    ],
+
+    'Gerente General': [
+        'DASHBOARD',
+        'RESERVA_LIST',
+        'HABITACION_LIST',
+        'TURNOS',
+        'TARIFA_LIST',
+    ],
+
+    'Gerente de Habitaciones': [
+        'RESERVA_CREATE',
+        'RESERVA_LIST',
+        'HABITACION_LIST',
+    ],
+
+    'Gerente de Comidas y Bebidas': [
+        'RESERVA_RESTAURANTE_CREATE',
+        'RESERVA_RESTAURANTE_LIST',
+        'TURNOS',
+    ],
+
+    'Asistente Administrativo': [
+        'RESERVA_CREATE',
+        'RESERVA_LIST',
+        'HABITACION_LIST',
+        'RESERVA_RESTAURANTE_LIST',
+        'TURNOS',
+        'TARIFA_LIST',
+    ],
+}
