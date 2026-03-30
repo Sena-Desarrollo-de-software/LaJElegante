c10ce23ac8db05f734f2dd8572bcea1633c4fdd4
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-29 20:24:31 -0500

    fix(backoffice): crear vistas temporales para shortcut
    - se decide crear unas pequeñas vistas temporales para usar en shortcuts
    - vistas vacias por el momento

--- backoffice/views.py
+++ backoffice/views.py
@@ -4,3 +4,13 @@ from django.views.decorators.http import require_GET,require_http_methods
 @require_GET
 def dashboard(request):
     return render(request,'backoffice/dashboard.html')
+
+@require_GET
+def create_reserva(request):
+    #Por ahora vacia, se implementara despues
+    pass
+
+@require_GET
+def index_reserva(request):
+    #Por ahora vacia, se implementara despues
+    pass
