5930874b7ffd90b403aa22be7efeb65ef94c93cd
Jeremy-Duarte <jeremydamian.db@gmail.com>
2026-03-31 22:55:01 -0500

    refactor(rooms): agregar metodo restore
    - Se refactoriza la logica de vistas para ser mas simple, usar decorators para mas seguridad
    - Nuevas urls para metodos
    - Formulario para restore
    - Refactorizacion general de la logica para usar nuevo BaseAudit model

--- rooms/forms.py
+++ rooms/forms.py
@@ -8,9 +8,10 @@ class HabitacionCreateForm(forms.ModelForm):
 
     def clean_numero_habitacion(self):
         num = self.cleaned_data["numero_habitacion"]
-        # Como el modelo tiene unique=True, NO se puede repetir ni aunque esté inactiva
-        if Habitacion.objects.filter(numero_habitacion=num).exists():
-            raise forms.ValidationError("Ya existe una habitación con ese número.")
+
+        if Habitacion.all_objects.filter(numero_habitacion=num).exists():
+            raise forms.ValidationError("Ya existe una habitación con ese número (activa o eliminada).")
+
         return num
 
 
@@ -21,17 +22,51 @@ class HabitacionUpdateForm(forms.ModelForm):
 
     def clean_numero_habitacion(self):
         num = self.cleaned_data["numero_habitacion"]
-        qs = Habitacion.objects.filter(numero_habitacion=num, is_active=True)
+        qs = Habitacion.objects.filter(numero_habitacion=num)
+
         if self.instance and self.instance.pk:
             qs = qs.exclude(pk=self.instance.pk)
+
         if qs.exists():
             raise forms.ValidationError("Ya existe una habitación activa con ese número.")
+
         return num
 
 
 class HabitacionDeleteForm(forms.Form):
+    confirm = forms.CharField(
+        required=True,
+        label="Escribe el número de la habitación para confirmar"
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.habitacion = kwargs.pop("habitacion")
+        super().__init__(*args, **kwargs)
+
+    def clean_confirm(self):
+        value = self.cleaned_data["confirm"]
+
+        if str(self.habitacion.numero_habitacion) != value:
+            raise forms.ValidationError("El número no coincide.")
+
+        return value
+
+class HabitacionRestoreForm(forms.Form):
     confirm = forms.BooleanField(
         required=True,
-        label="Confirmo que deseo eliminar (soft delete) esta habitación"
+        label="Confirmo que deseo restaurar esta habitación"
     )
-    
+
+    def __init__(self, *args, **kwargs):
+        self.habitacion = kwargs.pop("habitacion")
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if Habitacion.objects.filter(
+            numero_habitacion=self.habitacion.numero_habitacion
+        ).exists():
+            raise forms.ValidationError("Ya existe una habitación activa con este número.")
+
+        return cleaned_data
